8a4e4ed879c65631c78a24a2f7b97e3d0744c071
mridulwaldia <mridulsinghwaldia@gmail.com>
2018-06-27 14:32:23 -0500

    Edited Index a little bit

--- BoxProblem/BoxProblem/Controllers/BoxController.cs
+++ BoxProblem/BoxProblem/Controllers/BoxController.cs
@@ -26,7 +26,7 @@ namespace BoxProblem.Controllers
         // GET: Box/Create
         public ActionResult Create()
         {
-            return RedirectToAction("Index", "Create");
+            return View();
         }
 
         // POST: Box/Create
