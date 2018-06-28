508a9a2277dbb841eaada502a3066cc30b50b4f6
mridulwaldia <mridulsinghwaldia@gmail.com>
2018-06-28 11:26:21 -0500

    Revert "Merge remote-tracking branch 'origin/dev' into StyleSheet"
    This reverts commit 87e346df36b66472e02432b2435118ccdbf14a38.

--- BoxProblem/BoxProblem/Data/BoxInventory.cs
+++ BoxProblem/BoxProblem/Data/BoxInventory.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,23 +11,14 @@ namespace BoxProblem.Data
         [Key]
         public int Id { get; set; }
 
-        [Range(0,1000000)]
-        [DisplayName("WEIGHT")]
         public int Weight { get; set; }
 
-        [Range(0,1000000)]
-        [DisplayName("VOLUME")]
         public int Volume { get; set; }
 
-        [DisplayName("CAN HOLD LIQUID")]
         public bool CanHoldLiquid { get; set; }
 
-        [Range(0,1000000)]
-        [DisplayName("COST")]
         public double Cost { get; set; }
 
-        [Range(0,1000000)]
-        [DisplayName("INVENTORY COUNT")]
         public int InventoryCount { get; set; }
 
         public DateTime CreatedAt { get; set; }
