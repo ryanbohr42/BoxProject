6fc30962e053569dddfbe2e041c3f3881a528cf8
Ryan Bohr <ryanbohr42@hotmail.com>
2018-06-28 10:35:20 -0500

    done with input fixing

--- BoxProblem/BoxProblem/Data/BoxInventory.cs
+++ BoxProblem/BoxProblem/Data/BoxInventory.cs
@@ -11,14 +11,18 @@ namespace BoxProblem.Data
         [Key]
         public int Id { get; set; }
 
+        [Range(0,1000000)]
         public int Weight { get; set; }
 
+        [Range(0,1000000)]
         public int Volume { get; set; }
 
         public bool CanHoldLiquid { get; set; }
 
+        [Range(0,1000000)]
         public double Cost { get; set; }
 
+        [Range(0,1000000)]
         public int InventoryCount { get; set; }
 
         public DateTime CreatedAt { get; set; }
